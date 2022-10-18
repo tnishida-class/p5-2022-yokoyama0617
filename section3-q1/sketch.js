1e3070c483e1eb1f72a4fb1affcb8d3f9643e596
yokoyama0617 <114969333+yokoyama0617@users.noreply.github.com>
2022-10-18 12:23:30 +0900

    change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,30 @@
-// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-let count;
-let cycle;
+let x, y;
+let count, cycle;
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(windowWidth,windowHeight);
+  x = width / 2;
+  y = height / 2;
   count = 0;
-  cycle = 100;
+  cycle = 90;
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
 }
 
 function draw(){
-  background(160, 192, 255);
+  background(160, 192, 225);
+
   count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  console.log(count);
+  let size = 80 / count;
+  ellipse(width / 2, height / 2, size + x);
+
+  if(keyIsDown(ENTER)){
+  count = (count + 1.5) % cycle;
+  console.log(count);
+  let size = 80 / count;
+  ellipse(width / 2, height / 2, size + x);
+}
 }
