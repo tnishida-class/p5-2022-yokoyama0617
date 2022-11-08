16b27eeb23d744fc14d626bc4efa2e2b1755b00c
yokoyama0617 <114969333+yokoyama0617@users.noreply.github.com>
2022-11-08 11:25:41 +0900

    change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,16 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 0, 0, 0, 0, 0, 225, 0, 0);
 }
 
-function balloon(t){
+function balloon(t, x, y, r, g, b, r2, g2, b2){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  noStroke();
+  fill(r, g, b);
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(r2, g2, b2);
+  text(t, x + p, y + h);
 }
