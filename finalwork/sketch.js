8006d242c7d6909d3c26cd4093635b2e9dff3c94
yokoyama0617 <114969333+yokoyama0617@users.noreply.github.com>
2022-11-29 01:40:52 +0900

    change finalwork-final

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,19 +1,14 @@
-// 一時保存
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  background(220);
   draw1();
   fill(0);
   shikaku(4,100,100,90);
   draw2();
-  fill(50,100,90);
-  draw3();
-  draw4();
 }
 
 function draw1(){
-  background(220);
-
   fill(50,100,90);
   noStroke();
   ellipse(100,100,200);
@@ -26,36 +21,22 @@ function shikaku(n, cx, cy, r){
   beginShape();
   for(let i = 0; i < n; i++){
     let theta = TWO_PI * i * 1 / n - HALF_PI;
-    let x = cx + cos(theta) * r;
-    let y = cy + sin(theta) * r;
-    vertex(x,y);
-    endShape(CLOSE);
+    let x1 = cx + cos(theta) * r;
+    let y1 = cy + sin(theta) * r;
+    vertex(x1,y1);
   }
+  endShape(CLOSE);
 }
 
 function draw2(){
   fill(160,192,255);
   rect(37,37,126,126);
 }
-
-function draw3(){
-
-  let x = random(42,121);
-  let y = random(42,121);
-  let vx = random(3,5);
-  let vy = random(3,5);
-
-  ellipse(x,y,10);
-
-  x += vx;
-  y += vy;
-
-  if(x < 42 || x > 121){ vx = -1 * vx; }
-  if(y < 42 || y > 121){ vy = -1 * vy; }
-}
   
-function draw4(){
-  fill(0);
+function draw(){
+  if(mouseIsPressed){
+    fill(22,225,0);
+  }else{fill(0);}
   textSize(18);
   textFont("serif");
   text("Department of", 42, 100);
